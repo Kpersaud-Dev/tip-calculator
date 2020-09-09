f9fa146f3132dfffee11f66fad9cc30b0339b71a
kevin_p <kprsd14@gmail.com>
2020-09-09 17:03:12 -0400

    Added CSS and JS

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const elems = document.querySelectorAll('select');
-  const instances = M.FormSelect.init(elems, options);
-});
+//Caluclate Tip
+
+function calculateTip() {
+  const billAmount = document.getElementById('bill-amount').value,
+    serviceRating = document.getElementById('service-rating').value,
+    numberOfPeople = document.getElementById('number-of-people').value;
+  // Validate input
+  if (billAmount < 0) {
+    alert('Please enter valid value');
+  }
+
+  // if (billAmount === '' || numberOfPeople === 0 || serviceRating === '') {
+  //   alert('Please enter values');
+  // }
+
+  let total = (billAmount * serviceRating) / numberOfPeople;
+  // Round to two decimal places
+  total = Math.round(total * 100) / 100;
+  // Allow to have two digits after decimal point
+  total = total.toFixed(2);
+  // Display tip
+  document.getElementById('total-tip').style.display = 'block';
+  document.getElementById('tip').innerHTML = total;
+}
+
+// Hide tip amount on load
+document.getElementById('total-tip').style.display = 'none';
+
+// Click to call function
+document.getElementById('calculate').addEventListener('click', function () {
+  calculateTip();
+})
